c32c95d549dcaf6c963d9c4b05926b41c85baeb5
Fenrir-00 <96144320+Fenrir-00@users.noreply.github.com>
2022-08-20 20:39:41 +0200

    Update venon.py

--- venon.py
+++ venon.py
@@ -25,7 +25,7 @@ r= requests.get("https://raw.githubusercontent.com/Fenrir-00/investigar-web/main
 r=r.text
 print(r)
 if r != "version=1.3\n":
- os.system("exit")
+ sys.exit()
  print(f"""{color.rojo}HAY UNA NUEVA VERSION ACTUALIZA EL REPOSITORIO
 HAY UNA NUEVA VERSION ACTUALIZA EL REPOSITORIO
 HAY UNA NUEVA VERSION ACTUALIZA EL REPOSITORIO
